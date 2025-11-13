1fd1702f1e3c3154070af585201b6311c81a86f7
Theresa Dunca <146927903+tdunca@users.noreply.github.com>
2025-11-13 15:46:56 +0100

    Wrote and created GitHubProjectRepo.cs

---
+++ GitHubProjektRepo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace Lab4
+{
+    public class GitHubProjektRepo
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("description")]
+        public string Description { get; set; }
+
+        [JsonPropertyName("html_url")]
+        public string HtmlUrl { get; set; }
+
+        [JsonPropertyName("homepage")]
+        public string Homepage { get; set; }
+
+        [JsonPropertyName("watchers")]
+        public int Watchers { get; set; }
+
+        [JsonPropertyName("pushed_at")]
+        public DateTime PushedAt { get; set; }
+    }
+
+    public class ZipCodeInfo
+    {
+        [JsonPropertyName("places")]
+        public List<Place> Places { get; set; }
+
+        [JsonPropertyName("post code")]
+        public string PostCode { get; set; }
+    }
+
+    public class Place
+    {
+        [JsonPropertyName("place name")]
+        public string PlaceName { get; set; }
+
+        [JsonPropertyName("state")]
+        public string State { get; set; }
+
+        [JsonPropertyName("latitude")]
+        public string Latitude { get; set; }
+
+        [JsonPropertyName("longitude")]
+        public string Longitude { get; set; }
+    }
+
+}
